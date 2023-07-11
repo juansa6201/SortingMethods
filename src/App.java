ad184c15296ba76dec674c7f8b1191a6b8ad5782
Juan Cruz Mare <jcmare18@gmail.com>
2023-07-11 15:17:53 -0300

    printArray function

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
+    static void printArray(int array[])
+    {
+        int n = array.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bienvenidos a 'Taller de Algoritmos y Estructura de Datos I'");
     }
 }
